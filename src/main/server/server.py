a36847295a192fd56ffc75efad8be9dd6b9b2ffd
Luis <luisftomasprado@gmail.com>
2024-04-08 23:15:07 -0300

    feat: Implementing the event handling class, and fixing the event routes for the actual EventHandler functions to validate the API

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
+from src.models.settings.connection import db_connection_handler
 from src.main.routes.event_routes import event_route_bp
+
+db_connection_handler.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
